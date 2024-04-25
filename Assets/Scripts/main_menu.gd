4c354f79801917ddaaae625e8702b8b538eea1e5
anthony-a936 <117193836+anthony-a936@users.noreply.github.com>
2024-04-25 10:14:22 -0400

    volume for music
    game music player added to autoload in project settings
    volume controls added to main menu

--- Assets/Scripts/main_menu.gd
+++ Assets/Scripts/main_menu.gd
@@ -3,6 +3,9 @@ extends Control
 @export var gameroot : PackedScene
 
 @onready var setting_menu = %SettingsContainer
+@onready var volume_slider = %VolumeSlider
+@onready var game_music = get_node("/root/GameMusicPlayer")
+
 
 func _on_play_button_pressed():
 	var new_game = gameroot.instantiate()
@@ -10,6 +13,7 @@ func _on_play_button_pressed():
 
 
 func _on_settings_button_pressed():
+	volume_slider.value = game_music.volume_db
 	setting_menu.show()
 
 
@@ -20,3 +24,7 @@ func _on_quit_button_pressed():
 #Settings menu button
 func _on_close_button_pressed():
 	setting_menu.hide()
+
+
+func _on_h_slider_value_changed(value):
+	game_music.volume_db = value
