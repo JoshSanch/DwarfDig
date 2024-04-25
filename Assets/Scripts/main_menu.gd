43b7106cc1613abd3cd832e1080fda35856aa8f1
anthony-a936 <117193836+anthony-a936@users.noreply.github.com>
2024-04-24 22:59:23 -0400

    main menu started
    main menu art added
    main menu scene started
    main menu script created with buttons "on_pressed" signals attached.

---
+++ Assets/Scripts/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+
+func _on_play_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_settings_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	pass # Replace with function body.
